c8b359ad7e6ef8ed216e556533cbf5d348ec14fc
vlad <balabanovvladislav00@gmail.com>
2024-03-29 11:13:36 +0400

    rff

--- src/App.tsx
+++ src/App.tsx
@@ -9,7 +9,7 @@ function App() {
         <img src={logo} className="App-logo" alt="logo" />
         <h1>Hello world</h1>
         <p>
-        Hello world2 <code>src/App.tsx</code> and save to reload.
+        Hello redsdf <code>src/App.tsx</code> and save to reload.
         </p>
         <a
           className="App-link"
