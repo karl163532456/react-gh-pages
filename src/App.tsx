f85434773185b346b3d6cc4ab3a893a2d670de15
karl163532456 <balabanovvladislav00@gmail.com>
2024-04-02 16:05:40 +0400

    sew1

--- src/App.tsx
+++ src/App.tsx
@@ -9,7 +9,7 @@ function App() {
         <img src={logo} className="App-logo" alt="logo" />
         <h1>Hello world</h1>
         <p>
-        Hello world2
+        Hello world23
         f322 <code>src/App.tsx</code> and save to reload.
         </p>
         <a
